14af60ca1af6c227c38aabe3a9477390f8dfce6b
anastasia <legkova.anastasia127@gmail.com>
2024-04-11 16:47:05 +0300

    styled-footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import {SectionTitle} from "../../components/SectionTitle";
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {theme} from "../../styles/Theme";
 
 export const Footer = () => {
     return (
@@ -12,12 +13,12 @@ export const Footer = () => {
                 <SocialList>
                     <SocialItem>
                         <SocialLink>
-                            <Icon iconId={'in'}/>
+                            <Icon iconId={'in'} viewBox={'-3 -3 30 30'}/>
                         </SocialLink>
                     </SocialItem>
                     <SocialItem>
                         <SocialLink>
-                            <Icon iconId={'tg'}/>
+                            <Icon iconId={'tg'} viewBox={'-5 -4 33 33'}/>
                         </SocialLink>
                     </SocialItem>
                 </SocialList>
@@ -28,17 +29,21 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-    min-height: 10vh;
-    background-color: #ecc6ab;
+    padding: 40px 0 40px;
 `
 
 const Name = styled.h4`
-
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 700;
+    font-size: 22px;
+    letter-spacing: 2px;
+    
 `
 
 const SocialList = styled.ul`
     display: flex;
     gap: 20px;
+    margin: 30px 0;
 `
 
 const SocialItem = styled.li`
@@ -46,9 +51,25 @@ const SocialItem = styled.li`
 `
 
 const SocialLink = styled.a`
-
+    background-color: rgba(255, 255, 255, 0.1);
+    border-radius: 50%;
+    width: 35px;
+    height: 35px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    color: ${theme.colors.secondary};
+    
+    &:hover {
+        color: ${theme.colors.primary};
+        transform: translateY(-4px);
+    }
 `
 
 const Copyright = styled.small`
-
+    font-weight: 400;
+    font-size: 12px;
+    text-align: center;
+    opacity: 0.5;
 `
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -22,6 +22,7 @@ export const GlobalStyles = createGlobalStyle`
 
     a {
         text-decoration: none;
+        cursor: pointer;
     }
 
     ul {
